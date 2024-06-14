bf7aec94472cff70097eebe4fb6a363cb38e745b
tarunkumar456 <tk8657270@gmail.com>
2024-06-14 22:57:27 +0530

    minor change

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="flex-center   w-full bg-[#fbe6e9] bg-dotted-pattern bg-cover bg-fixed bg-center">
+        <div className="flex-center  min-h-screen  w-full bg-[#fbe6e9] bg-dotted-pattern bg-cover bg-fixed bg-center">
             {children}
         </div>
     )
